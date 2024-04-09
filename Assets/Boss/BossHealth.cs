b2e0336ad24b84a210d3f418ceb8f10ca56daa37
Jake D <jfdevaney23@gmail.com>
2024-04-09 06:24:26 -0400

    g

--- Assets/Boss/BossHealth.cs
+++ Assets/Boss/BossHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossHealth : MonoBehaviour
 {
@@ -14,6 +15,6 @@ public class BossHealth : MonoBehaviour
 
     private void die()
     {
-        Destroy(gameObject);
+        SceneManager.LoadScene("Level 2");
     }
 }
