6540e019e3a8ed41f95b4268f0bbf12f70d42f40
Jake D <jfdevaney23@gmail.com>
2024-04-09 00:15:35 -0400

    ?

--- Assets/UI/ScoreManager.cs
+++ Assets/UI/ScoreManager.cs
@@ -11,18 +11,6 @@ public class ScoreManager : MonoBehaviour
     private TextMeshProUGUI scoreText;
     public int score = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void UpdateScore(int pointsToAdd)
     {
         score += pointsToAdd;
