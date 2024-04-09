d7cf303f59482353f5b5ba2daafb64e5f8655196
Jake D <jfdevaney23@gmail.com>
2024-04-09 05:55:07 -0400

    add

--- Assets/Level 2/OilCollected.cs
+++ Assets/Level 2/OilCollected.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OilCollected : MonoBehaviour
 {
     public int oilCollected = 0;
+    
+    public int endThreshold = 100;
+    private DialogueTrigger dialogueTrigger;
+
+    private void Start()
+    {
+        dialogueTrigger = GetComponent<DialogueTrigger>();
+    }
+
+    private void Update()
+    {
+        if (oilCollected >= endThreshold)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
 }
